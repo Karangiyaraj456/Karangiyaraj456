839249044389d97d20cefef12799111c91e32857
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2024-03-21 20:59:26 +0530

    Create code 2.1 .cpp

---
+++ code 2.1 .cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+int i;
+cout<<"entara a value of i =";
+cin>>i;
+
+if(i==1)
+{
+    cout<<"entar a number is prime number ";
+}
+else if(i%2!=0)
+{
+     cout<<"entar a number is prime number ";
+}
+else{
+    cout<<"entar a number is not prime number ";
+}
+
+return 0;
+}
